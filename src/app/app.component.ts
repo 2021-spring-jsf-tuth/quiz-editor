e633a8fe304858a861ea8d481d2b5ae2a7548b32
heather kurth <hmkurth@madisoncollege.edu>
2021-04-15 12:23:31 -0500

    added error handling to observable
    the error div will only show if there is an error loading quizzes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,8 @@ export class AppComponent implements OnInit {
   ) {}
 
   quizzes = [];
-
+  errorLoadingQuizzes = false;
+  
   ngOnInit() {
     this.quizSvc
       .loadQuizzes()
@@ -30,7 +31,10 @@ export class AppComponent implements OnInit {
         }
 
         // Lamda with the errors, if errors exist
-        , (err) => console.error(err)
+        , (err) => {
+          console.error(err);
+        this.errorLoadingQuizzes = true;
+      }
 
       )
     ;
