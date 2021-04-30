21b5af5ac299aa56506bea18dcf5af25ec937cc8
Beck Anderson <bianderson@madisoncollege.edu>
2021-04-30 16:19:14 -0500

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,6 @@ import {
   , QuirkyShapeForSavingNewQuizzes 
 } from './quiz.service';
 
-import {
-  trigger
-  , transition
-  , animate
-  , keyframes
-  , style
-} from '@angular/animations';
-
 interface QuizDisplay {
   name: string;
 
@@ -46,29 +38,7 @@ type QuestionDisplay = {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('detailsFromLeft', [
-      transition('leftPosition => finalPosition', [
-        animate('300ms', keyframes([
-          style({ left: '-30px', offset: 0.0 }),
-          style({ left: '-20px', offset: 0.25 }),
-          style({ left: '-10px', offset: 0.5 }),
-          style({ left: '-5px', offset: 0.75 }),
-          style({ left: '0px', offset: 1.0 })
-        ]))
-      ]),
-    ]),
-    trigger('pulseSaveCancelButtons', [
-      transition('nothingToSave => somethingToSave', [
-        animate('400ms', keyframes([
-          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
-          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
-          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
-        ]))
-      ])
-    ])
-  ]  
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
@@ -115,7 +85,6 @@ export class AppComponent implements OnInit {
 
   selectQuiz(q) {
     this.selectedQuiz = q;
-    this.detailsFromLeftAnimationState = "finalPosition";
   }
 
   addNewQuiz() {
@@ -267,7 +236,7 @@ export class AppComponent implements OnInit {
 
       const newQuizzes: QuirkyShapeForSavingNewQuizzes[] = this.getNewlyAddedQuizzes().map(x => ({
         quizName: x.name
-        , quizQuestions: x.questions.map(y => ({question: y.name}))
+        , quizQuestions: x.questions.map(y => y.name)
       }));
 
       const numberOfEditedQuizzesSaved = await this.quizSvc.saveQuizzes(
